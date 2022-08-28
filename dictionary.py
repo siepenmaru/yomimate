cd83777dc2cf1e2b38c6c906a922edb79b03fdbe
Avatar Azka <avatar.azka@gmail.com>
2022-08-28 13:08:20 +1000

    feat: but not really. abandon get related kanji functionality

--- dictionary.py
+++ dictionary.py
@@ -39,19 +39,20 @@ class YomiDict(jamdict.Jamdict):
     def getKanjiRadicals(self, kanji: str):
         return self.krad[kanji]
 
-    def getRadicalKanji(self, radical: str):
-        result = self.lookup(''.join(self.radk[radical]))
-        dicts = []
-        for c in result.chars:
-            dicts.append(c.to_dict())
-        return dicts
+    # abandoned
+    def getRadicalKanji(self, radical: str) -> list[tuple]:
+        chars = self.radk[radical]
+        if chars:
+            result = self.lookup(''.join(chars))
+            charsOut = []
+            for c in result.chars:
+                meanings = []
+                if c.meanings():
+                    meanings = c.meanings()[0]
+                    charsOut.append((c, meanings))
+            return charsOut
+        return []
     
-    def getRelated(self) -> list[Character]:
-        entries = []
-        for ch in self.lookupResult.chars:
-            entries.append(ch)
-        return entries
-
     def toRomaji(self, original: str) -> str:
         result = self.kks.convert(original)
         out = ''
@@ -75,3 +76,22 @@ if __name__ == '__main__':
     # print all related characters
     for c in result.chars:
         print(c)
+
+    # Find all writing components (often called "radicals") of the character 雲
+    print(jam.krad['雲'])
+    # ['一', '雨', '二', '厶']
+
+    # Find all characters with the component 鼎
+    chars = jam.radk['鼎']
+    print(chars)
+    # {'鼏', '鼒', '鼐', '鼎', '鼑'}
+
+    # look up the characters info
+    result = jam.lookup(''.join(chars))
+    for c in result.chars:
+        print(c, c.meanings())
+    # 鼏 ['cover of tripod cauldron']
+    # 鼒 ['large tripod cauldron with small']
+    # 鼐 ['incense tripod']
+    # 鼎 ['three legged kettle']
+    # 鼑 []
